b84049702efff654114b803c95092dd0032b69dc
Russell Ahlstrom <russell.ahlstrom@gmail.com>
2013-10-21 22:39:06 -0600

    Alert New Reward Levels and Single Alert on Open
    Adds a reward cache so we can send out an alert on new reward levels
    opening up and only send a single alert when a reward level changes
    from 0 to open.

--- projectWatcher.rb
+++ projectWatcher.rb
@@ -33,7 +33,7 @@ if options[:send_email]
     Mail.defaults do
       delivery_method :smtp, {
         :address => 'smtp.gmail.com',
-        :port => '587',
+        :port => '875',
         :user_name => kickwatch_config['kickstarter_gamil_username'],
         :password => kickwatch_config['kickstarter_gmail_password'],
         :authentication => :plain,
@@ -46,13 +46,60 @@ client = Kickscraper.client
 
 project = client.find_project(project_config['slug'])
 
-project.rewards.each {|reward|
-    if project_config['rewards'].include? reward.id
-        reward.name = reward.reward.partition(':').first
+begin
+    old_rewards = YAML.load_file("cache/#{project_config['slug']}.yml")
+rescue
+    old_rewards = nil
+end
 
+project.rewards.each {|reward|
+    reward.name = reward.reward.partition(':').first
+    if old_rewards
+        previous_reward = old_rewards.select{|r| r.id == reward.id}.first
+    else
+        previous_reward = nil
+    end
+
+    if project_config['rewards'].include? reward.id
         if reward.remaining > 0
             subject = "Kickstarter Alert: Found #{reward.remaining} opening(s) for #{reward.name}"
             body = "#{reward.reward}\n\n#{reward.remaining} Left"
+
+            if options[:send_email]
+                if previous_reward.nil? || previous_reward.remaining == 0
+                    Mail.deliver do
+                       from     'no-reply@kickstater.com'
+                       to       kickwatch_config['kickstarter_email']
+                       subject  subject
+                       body     body
+                    end
+                end
+            else
+                if previous_reward.nil? || previous_reward.remaining == 0
+                    puts '==='
+                    puts subject
+                    puts body
+                    puts '==='
+                else
+                    puts "ALERT: #{reward.remaining} slot(s) open for #{reward.name}"
+                end
+            end
+        else
+            unless options[:send_email]
+                puts "No slots open for #{reward.name}"
+            end
+        end
+    end
+
+    if old_rewards
+        unless old_rewards.include? reward
+            subject = "Kickstart Alert: New Reward Level \"#{reward.name}\" Found!"
+            body = "#{reward.reward}"
+            if reward.remaining
+                body += "\n\n#{reward.remaining} Left"
+            else
+                body += "\n\nUnlimited Tier"
+            end
 
             if options[:send_email]
                 Mail.deliver do
@@ -67,10 +114,8 @@ project.rewards.each {|reward|
                 puts body
                 puts '==='
             end
-        else
-            unless options[:send_email]
-                puts "No slots open for #{reward.name}"
-            end
         end
     end
 };
+
+File.open("cache/#{project_config['slug']}.yml", 'w') {|f| f.write project.rewards.to_yaml }
