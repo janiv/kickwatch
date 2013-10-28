6a4c244147b393b875ac1e48c87e6c5bf26ba273
Russell Ahlstrom <russell.ahlstrom@gmail.com>
2013-10-28 10:16:01 -0600

    Removes Dice Vault
    The dice vault project has ended. Removes its project and schedule
    configs.

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,5 @@ job_type :local_command, 'cd :path && :task :output'
 set :output, {:error => 'logs/error.log', :standard => 'logs/success.log'}
 
 every 3.minutes do
-    local_command "./projectWatcher.rb -p dice_vault -e"
     local_command "./projectWatcher.rb -p shadows -e"
 end
