635cb6156e0e9dde81f2f84c8a877282f33de281
Russell Ahlstrom <russell.ahlstrom@gmail.com>
2013-12-04 21:46:35 -0700

    Remove shadows and keyflower

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,4 @@ job_type :local_command, 'cd :path && :task :output'
 set :output, {:error => 'logs/error.log', :standard => 'logs/success.log'}
 
 every 3.minutes do
-    local_command "./projectWatcher.rb -p shadows -e"
-    local_command "./projectWatcher.rb -p keyflower -e"
 end
