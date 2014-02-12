e9c93aab529391cfe268935a31fa8c97d31b30e9
Russell Ahlstrom <russell.ahlstrom@gmail.com>
2014-02-12 09:48:16 -0700

    Add gameframe to schedule config

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,4 +3,5 @@ job_type :local_command, 'cd :path && :task :output'
 set :output, {:error => 'logs/error.log', :standard => 'logs/success.log'}
 
 every 3.minutes do
+    local_command "./projectWatcher.rb -p gameframe -e"
 end
